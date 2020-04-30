601e6d2a8acef75c1dc1b57d8c5aaab3578329de
FidFenix <fid.mamani@gmail.com>
2020-04-29 22:58:51 -0500

    Initial setup for backend PHP (laravel) and mysql

---
+++ routes/api.php
@@ -0,0 +1,88 @@
+<?php
+
+use Dingo\Api\Routing\Router;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*
+ * Welcome route - link to any public API documentation here
+ */
+Route::get('/', function () {
+    echo 'Welcome to our API';
+});
+
+/** @var \Dingo\Api\Routing\Router $api */
+$api = app('Dingo\Api\Routing\Router');
+$api2 = app('Dingo\Api\Routing\Router');
+
+/*$api2->version('v2', function(Router $api2){
+    $api2->group(['prefix'=> 'hola'], function(Router $api2) {
+        echo 'Hola Fidel';
+    });
+});*/
+
+$api->version('v1', ['middleware' => ['api']], function (Router $api) {
+    /*
+     * Authentication
+     */
+    $api->group(['prefix' => 'auth'], function (Router $api) {
+        $api->group(['prefix' => 'jwt'], function (Router $api) {
+            $api->get('/token', 'App\Http\Controllers\Auth\AuthController@token');
+        });
+    });
+
+    /*
+    $api->group(['prefix' => 'authe'], function (Router $api) {
+        $api->group(['prefix' => 'jwt'], function (Router $api) {
+            $api->get('/token', function(){
+                echo api;
+            });
+        });
+    });*/
+
+    /*
+     * Authenticated routes
+     */
+    $api->group(['middleware' => ['api.auth']], function (Router $api) {
+        /*
+         * Authentication
+         */
+        $api->group(['prefix' => 'auth'], function (Router $api) {
+            $api->group(['prefix' => 'jwt'], function (Router $api) {
+                $api->get('/refresh', 'App\Http\Controllers\Auth\AuthController@refresh');
+                $api->delete('/token', 'App\Http\Controllers\Auth\AuthController@logout');
+            });
+
+            $api->get('/me', 'App\Http\Controllers\Auth\AuthController@getUser');
+        });
+
+        /*
+         * Users
+         */
+        $api->group(['prefix' => 'users', 'middleware' => 'check_role:admin'], function (Router $api) {
+            $api->get('/', 'App\Http\Controllers\UserController@getAll');
+            $api->get('/{uuid}', 'App\Http\Controllers\UserController@get');
+            $api->post('/', 'App\Http\Controllers\UserController@post');
+            $api->put('/{uuid}', 'App\Http\Controllers\UserController@put');
+            $api->patch('/{uuid}', 'App\Http\Controllers\UserController@patch');
+            $api->delete('/{uuid}', 'App\Http\Controllers\UserController@delete');
+        });
+
+        /*
+         * Roles
+         */
+        $api->group(['prefix' => 'roles'], function (Router $api) {
+            $api->get('/', 'App\Http\Controllers\RoleController@getAll');
+        });
+    });
+});
